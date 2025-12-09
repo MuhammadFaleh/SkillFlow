9c629914782795596133d7f78066d4ea1e5cfb1e
MuhammadFaleh <74877253+MuhammadFaleh@users.noreply.github.com>
2025-12-09 11:33:48 +0300

    new

---
+++ src/main/java/org/example/skillflow/Service/CompanyRequestService.java
@@ -0,0 +1,60 @@
+package org.example.skillflow.Service;
+
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.example.skillflow.API.APIException;
+import org.example.skillflow.Model.CompanyRequest;
+import org.example.skillflow.Model.Company_Request;
+import org.example.skillflow.Repository.Company_RequestRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CompanyRequestService {
+
+    private final Company_RequestRepository companyRequestRepository;
+
+    public List<Company_Request> getAllCompanyRequests(){
+        return companyRequestRepository.findAll();
+    }
+
+    public void addCompanyRequest(@Valid CompanyRequest companyRequest){
+        companyRequest.setRequestDate(LocalDate.now());
+        companyRequest.setEndDate(LocalDate.now().plusMonths(1));
+        companyRequest.setStatus("pending");
+        companyRequestRepository.save(companyRequest);
+    }
+
+    public void updateCompanyRequest(Integer id , @Valid CompanyRequest companyRequest){
+       Company_Request companyRequest1 = companyRequestRepository.findCompany_RequestById(id);
+       if (companyRequest1 == null){
+           throw new APIException("Company Request Not found");
+       }
+       if ("accepted".equalsIgnoreCase(companyRequest.getStatus())){
+           throw new APIException("the request is accepted");
+       }
+       companyRequest1.setCompanyName(companyRequest.getCompanyName());
+       companyRequest1.setCounty(companyRequest.getCounty());
+       companyRequest1.setFullName(companyRequest.getFullName());
+       companyRequest1.setIndustry(companyRequest.getIndustry());
+       companyRequest1.setRecordNumber(companyRequest.getRecordNumber());
+       companyRequest1.setRequestDate(LocalDate.now());
+       companyRequest1.setEndDate(LocalDate.now().plusMonths(1));
+       companyRequestRepository.save(companyRequest1);
+    }
+
+    public void deleteCompanyRequest(Integer id){
+        Company_Request companyRequest = companyRequestRepository.findCompany_RequestById(id);
+        if (companyRequest == null ){
+            throw new APIException("company request not found");
+        }
+        if (!companyRequest.getStatus().equalsIgnoreCase("pending")){
+            throw new APIException("only pending requests can be deleted");
+        }
+        companyRequestRepository.delete(companyRequest);
+    }
+}
--- src/main/java/org/example/skillflow/Service/EmployeeService.java
+++ src/main/java/org/example/skillflow/Service/EmployeeService.java
@@ -2,9 +2,34 @@ package org.example.skillflow.Service;
 
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.example.skillflow.DTO.In.EmployeeDTOIn;
+import org.example.skillflow.DTO.Out.EmployeeDTOOut;
+import org.example.skillflow.Model.Employee;
+import org.example.skillflow.Repository.EmployeeRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class EmployeeService {
+    private final EmployeeRepository employeeRepository;
+
+    public List<EmployeeDTOOut> getEmployees(){
+        List<EmployeeDTOOut> employeeDTOOuts = new ArrayList<>();
+        for (Employee employee : employeeRepository.findAll()){
+            employeeDTOOuts.add(convertToDTO(employee));
+        }
+        return employeeDTOOuts;
+    }
+
+    public void createEmployee(EmployeeDTOIn employeeDTOIn){
+        
+    }
+
+    public EmployeeDTOOut convertToDTO(Employee employee){
+        return new EmployeeDTOOut(employee.getId(),employee.getUsername(),employee.getFull_name(),
+                employee.getGender(),employee.getAge(),employee.getEmail());
+    }
 }
